8a141f43d0d64f6f15da708a322aa67550aba656
AugmentVista <AuAgments@gmail.com>
2022-12-20 22:17:53 -0400

    First Commit, Task list V1.0.0 created

---
+++ MTG SEDH score system/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTG_SEDH_score_system
+        // To do list:
+        // Create and Array for each catagory on a single card and fill them out 
+        // Decide which Arrays effect gameplay and which do not. for those that do each of them would be a new array setting them in order of strength. 
+        // 0 being the strongest. 
+        // Create a score method that looks through each array given to it and prints the point values for each catagory 
+        // create a CARDNAME method that scores every catagory present on one single card and test to see if the score method returns the expected result
+        // Test the upper and lower limits of the score method so that it cannot return unreasonable values such as negative values. 
+{
+    internal class Program
+    {
+
+
+
+
+
+
+
+
+
+    }
+}
